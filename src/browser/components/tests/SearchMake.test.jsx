8680120916e9f97982bc3c179d18c559ec110c2e
sutikshan dubey <s.anand.dubey@gmail.com>
2018-04-23 21:22:58 +1000

    add more detail tests.

--- src/browser/components/tests/SearchMake.test.jsx
+++ src/browser/components/tests/SearchMake.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { SearchMake } from "../SearchMake";
+import { stub } from "sinon";
 
 it("should render a SearchMake component correctly", () => {
   const data = {
@@ -9,4 +10,18 @@ it("should render a SearchMake component correctly", () => {
   const wrapper = shallow(<SearchMake data={data} onMakeSelect={() => {}} />);
 
   expect(wrapper).toMatchSnapshot();
+});
+
+it("should handle make change event", () => {
+  const data = {
+    makes: [{ id: 1, Name: "i30" }]
+  };
+
+  const makeSelectStub = stub();
+  const wrapper = shallow(
+    <SearchMake data={data} onMakeSelect={makeSelectStub} />
+  );
+
+  wrapper.find("select").simulate("change", { value: 1 });
+  expect(makeSelectStub.calledOnce).toBeTruthy();
 });
--- src/browser/components/tests/SearchModel.test.jsx
+++ src/browser/components/tests/SearchModel.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { SearchModel } from "../SearchModel";
+import { stub } from "sinon";
 
 it("should render a SearchModel component correctly", () => {
   const data = {
@@ -9,4 +10,18 @@ it("should render a SearchModel component correctly", () => {
   const wrapper = shallow(<SearchModel data={data} onModelSelect={() => {}} />);
 
   expect(wrapper).toMatchSnapshot();
+});
+
+it("should render a SearchModel component and handle model change event", () => {
+  const data = {
+    models: [{ id: 1, Name: "i30" }]
+  };
+
+  const modelSelectStub = stub();
+  const wrapper = shallow(
+    <SearchModel data={data} onModelSelect={modelSelectStub} />
+  );
+
+  wrapper.find("select").simulate("change", { value: 1 });
+  expect(modelSelectStub.calledOnce).toBeTruthy();
 });
